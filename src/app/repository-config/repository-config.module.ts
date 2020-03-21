98155e5426ba9941dfe87ff6205df160f84f8421
Alejandro Perdomo <aperdomob@psl.com.co>
2020-03-21 17:41:41 -0500

    feat(repository-config): create module and component base

---
+++ src/app/repository-config/repository-config.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RepositoryListComponent } from './components/repository-list/repository-list.component';
+
+
+
+@NgModule({
+  declarations: [RepositoryListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class RepositoryConfigModule { }
