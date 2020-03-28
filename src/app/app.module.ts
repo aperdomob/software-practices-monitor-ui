ad45f181a47aeb21bd1299757d8f9b68cfac9742
Hetzel Cordoba <hetzel30@gmail.com>
2020-03-28 18:37:07 -0500

    feat(dashboard): add dashboard
    Add scss preprocessors
    Add bootstrap grid library
    Add font awesome library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MonitorServicesModule } from './monitor-services/monitor-services.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MonitorServicesModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/monitor-services/components/monitor-services-detail/monitor-services-detail.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IItegration } from '../../interfaces/IItegration';
+import { IntegrationsService } from '../../services/integrations/integrations.service';
+import { Label, MultiDataSet } from 'ng2-charts';
+import { ChartType } from 'chart.js';
+
+@Component({
+  selector: 'app-monitor-services-detail',
+  templateUrl: './monitor-services-detail.component.html',
+  styleUrls: ['./monitor-services-detail.component.scss'],
+})
+export class MonitorServicesDetailComponent implements OnInit {
+  @Input() integration: IItegration;
+  public statistics;
+
+  // Doughnut
+  public doughnutChartLabels: Label[] = ['Ok', 'Errors'];
+  public doughnutChartData: MultiDataSet = [[350, 450]];
+  public doughnutChartType: ChartType = 'doughnut';
+  public pieChartColors = [
+    {
+      backgroundColor: ['#8DD30B', '#F83F3C'],
+    },
+  ];
+
+  public successCode = 'OK';
+  public errorCode = 'FAILED';
+
+  constructor(private readonly integrationsService: IntegrationsService) {}
+
+  ngOnInit(): void {
+    this.setStatisticsById(this.integration.id);
+  }
+
+  setStatisticsById(id: string) {
+    console.log(id);
+    this.integrationsService.getStatisticsById(id).subscribe((result) => {
+      this.statistics = result;
+      this.doughnutChartData = [[this.statistics.ok, this.statistics.fail]];
+    });
+  }
+
+  changeIntegration(integration: IItegration) {
+    this.integration = integration;
+    this.setStatisticsById(this.integration.id);
+  }
+}
