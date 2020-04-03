12d24a1550efa38b2896c390dfc9b92677c05af5
Alejandro Perdomo <aperdomob@psl.com.co>
2020-04-02 21:23:05 -0500

    refactor(clean): remove unused module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'software-practices-monitor-ui';
+  title = 'Software Practices Monitor';
 }
--- src/app/repository-config/repository-config.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RepositoryListComponent } from './components/repository-list/repository-list.component';
-
-@NgModule({
-  declarations: [RepositoryListComponent],
-  imports: [CommonModule],
-})
-export class RepositoryConfigModule {}
