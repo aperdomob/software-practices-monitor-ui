25e6ce0403e9fa9e9dc5a8e933fe374f128d2c45
Alejandro Perdomo <aperdomob@psl.com.co>
2020-04-06 20:16:20 -0500

    feat(github-overview): link overview with detail view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MonitorServicesListComponent } from './monitor-services/components/monitor-services-list/monitor-services-list.component';
+import { MonitorServicesDetailComponent } from './monitor-services/components/monitor-services-detail/monitor-services-detail.component';
+import { MonitorGithubOverviewComponent } from './monitor-services/components/monitor-github-overview/monitor-github-overview.component';
 
 const routes: Routes = [
-  {
-    path: 'dashboard',
-    data: { breadcrumb: 'Dashboard' },
-    children: [
-      {
-        path: '',
-        component: MonitorServicesListComponent,
-        pathMatch: 'full',
-      },
-    ],
-  },
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'dashboard',
-  },
-  {
-    path: '**',
-    redirectTo: 'dashboard',
-  },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full', data: { breadcrumb: 'Dashboard' } },
+  { path: 'dashboard', component: MonitorServicesListComponent },
+  { path: 'github-overview', component: MonitorGithubOverviewComponent },
+  { path: 'github-overview/:id', component: MonitorServicesDetailComponent },
 ];
 
 @NgModule({
