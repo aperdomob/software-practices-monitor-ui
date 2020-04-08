d01f3f4d53ad15c587665829b3f8673793a7ba7e
Hetzel Cordoba <hcordobab@psl.com.co>
2020-04-07 21:34:41 -0500

    feat(git): new routing/theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MonitorServicesListComponent } from './monitor-services/components/monitor-services-list/monitor-services-list.component';
-import { MonitorServicesDetailComponent } from './monitor-services/components/monitor-services-detail/monitor-services-detail.component';
 import { MonitorGithubOverviewComponent } from './monitor-services/components/monitor-github-overview/monitor-github-overview.component';
+import { MonitorServicesDetailComponent } from './monitor-services/components/monitor-services-detail/monitor-services-detail.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full', data: { breadcrumb: 'Dashboard' } },
-  { path: 'dashboard', component: MonitorServicesListComponent },
-  { path: 'github-overview', component: MonitorGithubOverviewComponent },
-  { path: 'github-overview/:id', component: MonitorServicesDetailComponent },
+  {
+    path: 'dashboard',
+    children: [
+      {
+        path: '',
+        component: MonitorServicesListComponent,
+        pathMatch: 'full',
+      },
+      { path: 'github-overview', component: MonitorGithubOverviewComponent },
+      { path: 'github-overview/:id', component: MonitorServicesDetailComponent },
+    ],
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard',
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
 ];
 
 @NgModule({
